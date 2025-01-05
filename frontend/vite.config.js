b36cda6a459acabbc07f0c10b5769813a3e1341a
HansajithNilan <nilanhansajith7@gmail.com>
2025-01-05 21:46:17 +0530

    Token Secret

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FabNest-Online-store/', // Replace with your repo name
+  build: {
+    outDir: 'dist',
+  },
 })
