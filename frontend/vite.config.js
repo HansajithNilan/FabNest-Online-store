2754b2dc03a5b54db42b506afda594e4060932eb
HansajithNilan <nilanhansajith7@gmail.com>
2025-01-06 01:20:25 +0530

    Local Host Error Fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FabNest-Online-store/', // Replace with your repo name
-  build: {
-    outDir: 'dist',
-  },
+  base: '/', // Replace with your repo name
+
 })
